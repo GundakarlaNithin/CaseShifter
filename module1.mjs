1887c012cb12c8e63e044a9ba8734dddc1a31435
Gundakarla Nithin <163517157+GundakarlaNithin@users.noreply.github.com>
2024-10-17 15:55:02 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,6 @@
+import a from './module2.mjs';
+import u, {b,d,c} from './module2.mjs'
+console.log(b);
+console.log(d);
+console.log(c);
+console.log(u);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Nani";
+const b = "Amma";
+const c = "Daddy";
+const d = "Chelle";
+
+export default a;
+export {b};
+export {c};
+export {d};
